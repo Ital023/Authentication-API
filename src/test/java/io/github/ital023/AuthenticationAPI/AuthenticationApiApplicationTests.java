6094b538f82bb348160b5013776b214ac8207bd2
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-20 21:13:46 -0300

    feat: initial files

---
+++ src/test/java/io/github/ital023/AuthenticationAPI/AuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ital023.AuthenticationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
