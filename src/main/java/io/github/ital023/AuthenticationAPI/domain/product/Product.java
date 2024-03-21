8b9cb47444ce3a2643d63c7aa5333aae24d26787
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-20 21:27:04 -0300

    feat: Product Entity and migration(FlyWay)

---
+++ src/main/java/io/github/ital023/AuthenticationAPI/domain/product/Product.java
@@ -0,0 +1,27 @@
+package io.github.ital023.AuthenticationAPI.domain.product;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+
+@Table(name = "product")
+@Entity(name = "product")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Integer price;
+
+}
