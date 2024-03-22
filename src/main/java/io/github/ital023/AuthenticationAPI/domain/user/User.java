8e5724afff8027ea19f0e5899e97ce36405676e2
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-21 21:23:43 -0300

    feat: User entity

---
+++ src/main/java/io/github/ital023/AuthenticationAPI/domain/user/User.java
@@ -0,0 +1,52 @@
+package io.github.ital023.AuthenticationAPI.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails {
+    private String id;
+    private String login;
+    private String password;
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return getLogin();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
