419df049c79f3d424408a97d2ff8aefe9b6513f8
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-03-21 21:29:40 -0300

    feat: User Enum

---
+++ src/main/java/io/github/ital023/AuthenticationAPI/domain/user/UserRole.java
@@ -0,0 +1,14 @@
+package io.github.ital023.AuthenticationAPI.domain.user;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole(){return role;}
+}
